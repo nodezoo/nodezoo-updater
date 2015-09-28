725638c7d6992d32b4216d202007b7af942d4f59
Richard Rodger <richard@ricebridge.com>
2015-09-28 23:25:00 +0100

    process modules

--- npm-update.js
+++ npm-update.js
@@ -11,36 +11,72 @@ module.exports = function npm_update( options ){
   var seneca = this
 
   options = seneca.util.deepextend({
+    batchsize: 1,
     registry: 'http://registry.npmjs.org/'
   },options)
 
 
   //seneca.add( 'role:npm,task:registry_subscribe', registry_subscribe )
-  //seneca.add( 'role:npm,task:process_modules',    process_modules )
+  seneca.add( 'role:npm,task:process_modules',    process_modules )
   seneca.add( 'role:npm,task:download_modules',   download_modules )
 
 
   var state = {
-    download_modules: { status:'stop' }
+    download_modules: { status:'stop' },
+    process_modules: { status:'stop' }
   }
   
-  function download_modules( msg, respond ) {
+
+  function process_modules( msg, respond ) {
     var seneca = this
 
-    if( 'stop' === msg.control ) {
-      state.download_modules.status = 'stop'
-    }
+    process_control( 
+      msg, respond, seneca, 'process_modules', 
+      function( msg, respond, error ){
+        
+        state.process_modules.status = 'run'
+        
+        seneca.make('mod').list$({done:false},function(err,list){
+          if(err) return error(err)
 
-    else if( 'status' === msg.get || 
-             'run' === state.download_modules.status ) 
-    {
-      // do nothing
-    }
+          state.process_modules.done = 0
+          state.process_modules.total = list.length
 
-    // run download and update
-    else {
-      state.download_modules.status = 'run'
-      process.nextTick(function(){
+          async.eachLimit( list, options.batchsize, function(mod,done) {
+            var name = mod.id
+            seneca.act('role:npm,info:change',{name:name})
+
+            seneca.make('mod',{id$:name}).load$(function(err,mod){
+              if( err ) return error(err)
+
+              mod = mod || seneca.make('mod',{id$:name})
+              mod.done = true
+
+              mod.save$( function( err, mod ){
+                if( err ) return error(err)
+
+                state.process_modules.done++
+
+                done()
+              })
+            })
+          },function(err){
+            if(err) return error(err)
+          })
+        })
+      })
+  }
+
+
+  function download_modules( msg, respond ) {
+    var seneca = this
+    
+    process_control( 
+      msg, respond, seneca, 'download_modules', 
+      function( msg, respond, error ){
+
+        state.download_modules.status = 'run'
+
         var stats = npm_stats()
         stats.list({}, function(err,list){
           if(err) return error(err)
@@ -48,8 +84,8 @@ module.exports = function npm_update( options ){
           state.download_modules.done = 0
           state.download_modules.total = list.length
 
-          async.eachLimit( list, 1, function(mod,done) {
-            seneca.make('mod',{id$:mod,done:false}).save$(function(err,mod){
+          async.eachLimit( list, options.batchsize, function(name,done) {
+            seneca.make('mod',{id$:name,done:false}).save$(function(err,mod){
               if( err ) return done(err)
               state.download_modules.done++
               done()
@@ -59,17 +95,33 @@ module.exports = function npm_update( options ){
           })
         })
       })
-    }
+  }
 
-    return respond( null, state.download_modules )
+
+  function process_control( msg, respond, seneca, process_name, worker ) {
 
     function error(err) {
       seneca.log(err)
-      state.download_modules.status = 'error'
-      state.download_modules.error = err
+      state[process_name].status = 'error'
+      state[process_name].error = err
     }
-  }
 
+    if( 'stop' === msg.control ) {
+      state[process_name].status = 'stop'
+    }
+
+    else if( 'status' === msg.get || 
+             'run' === state[process_name].status ) 
+    {
+      // do nothing
+    }
+
+    else if( worker( msg, respond, error ) ) {
+      // already responded
+    }
+
+    return respond( null, state[process_name] )
+  }
 
 
   seneca.add({init:'npm-update'}, function(args,done){
--- test/local-npm-update.js
+++ test/local-npm-update.js
@@ -1,6 +1,11 @@
 require('seneca')()
   .use('level-store')
   .use('..')
+
+  .add('role:npm,info:change',function( msg, respond ){
+    console.log( this.util.clean(msg) )
+    respond()
+  })
+
   .listen(9001)
   .repl()
-
