9285ffac12ee022122abacf2b72279955f63b6f8
Richard Rodger <richard@ricebridge.com>
2015-12-09 21:05:34 +0000

    mesh

--- srv/update-dev.js
+++ srv/update-dev.js
@@ -1,18 +1,15 @@
 
-var BEANSTALK = process.env.BEANSTALK || 'localhost'
 var STATS = process.env.STATS || 'localhost'
 
 require('seneca')()
   .use('level-store')
-  .use('beanstalk-transport')
   .use('msgstats',{
     udp: { host: STATS },
     pin:'role:npm,info:change'
   })
   .use('../npm-update.js')
 
-  .client({ host:BEANSTALK, pin:'role:npm,info:change',type:'beanstalk' })
+  .use( 'mesh', {auto:true} )
 
-  .listen(44005)
-  .repl(43005)
+  .repl(33005)
 
