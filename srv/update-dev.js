0df8190f6e0a684058f607994fd9634be42fdcfc
Gege Pincin <gege@pincin.com>
2016-04-13 16:23:27 -0400

    upgrade seneca, install redisQ & redisStore

--- srv/update-dev.js
+++ srv/update-dev.js
@@ -1,18 +1,49 @@
+var Seneca = require('seneca')
+var Entities = require('seneca-entity')
+var RedisStore = require('seneca-redis-store')
+var RedisQ = require('seneca-redis-queue-transport')
+var MsgStats = require('seneca-msgstats')
 
-var BEANSTALK = process.env.BEANSTALK || 'localhost'
 var STATS = process.env.STATS || 'localhost'
 
-require('seneca')()
-  .use('level-store')
-  .use('beanstalk-transport')
-  .use('msgstats',{
-    udp: { host: STATS },
-    pin:'role:npm,info:change'
-  })
-  .use('../npm-update.js')
+var opts = {
+  redisStore: {
+    host: 'localhost',
+    port: 6379
+  },
+  redisQ: {
+      timeout: 22222,
+      type: 'redis-queue',
+      host: 'localhost',
+      port: 6379
+    },
+    msgStats: {
+      udp: { host:STATS },
+      pin: 'role:npm, info:change'
+    }
+}
 
-  .client({ host:BEANSTALK, pin:'role:npm,info:change',type:'beanstalk' })
+var Service = Seneca(opts.seneca)
 
-  .listen(44005)
-  .repl(43005)
+Service.use(Entities)
+       .use(RedisStore, opts.redisStore)
+       .use(RedisQ, opts.redisQ)
+       .use(MsgStats, opts.msgStats)
+       .use('../npm-update-new.js')
+       .listen(44005)
+       .repl(43005)
 
+// require('seneca')()
+//   .use('redis-store', opts['redis-store'])
+//   .use('redis-queue-transport', opts['redis-queue'])
+//   .use('msgstats',{
+//     udp: { host: STATS },
+//     pin:'role:npm,info:change'
+//   })
+//
+//   .use('../npm-update-new.js')
+
+  // .client({ host:BEANSTALK, pin:'role:npm,info:change',type:'beanstalk' })
+  //
+  // .listen(44005)
+  // .repl(43005)
