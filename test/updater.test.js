09d592412c560a9844fc258fc87bfe7730b247fe
Gege Pincin <gege@pincin.com>
2016-04-20 23:37:52 -0400

    part of the tests

--- test/updater.test.js
+++ test/updater.test.js
@@ -3,7 +3,25 @@
 var Code = require('code')
 var Lab = require('lab')
 var Seneca = require('seneca')
-var Dequeue = require('../lib/updater')
+var Sinon = require('sinon')
+var Proxy = require('proxyquire')
+var Through2 = require('through2')
+
+var npmStatsObj = {list: function () {}}
+Sinon.stub(npmStatsObj, 'list', function () {
+  var stream = Through2()
+  setImmediate(() => {
+    stream.write('[')
+    stream.write('"honeybadger",')
+    stream.write('"dont",')
+    stream.write('"care",')
+    stream.write(']')
+  })
+  return stream
+})
+var npmStats = Sinon.stub().returns(npmStatsObj)
+
+var Dequeue = Proxy('../lib/updater', {'npm-stats': npmStats})
 
 var lab = exports.lab = Lab.script()
 var expect = Code.expect
@@ -13,7 +31,6 @@ var it = lab.it
 var describe = lab.describe
 
 var Service = Seneca()
-
 Service.use(Dequeue)
 
 suite('nodezoo-updater suite tests ', function () {
@@ -22,24 +39,47 @@ suite('nodezoo-updater suite tests ', function () {
       if (err) {
         return process.exit(!console.error(err))
       }
-      Service.add('role:info,req:part', function (args, done) {
+      Service.add('role:updater,info:update', function (msg, done) {
         done()
       })
-      done()
+      done(err)
     })
   })
 })
 
 describe('nodezoo-updater tests', () => {
-  it('Plugin Recieves Messages', function (done) {
-    Service.act({role: 'updater', info: 'update', name: 'honeybadger'}, function (err, respond) {
+  it('Registry Subscribe Responds', function (done) {
+    Service.act({role: 'npm', cmd: 'registrySubscribe'}, function (err, respond) {
       expect(err).to.not.exist()
+      expect(respond).to.exist()
       done(err)
     })
   })
-  it('Plugin Responds to Messages', function (done) {
-    Service.act({role: 'updater', info: 'update', name: 'honeybadger'}, function (err, respond) {
+  it('Registry Unsubscribe Responds', function (done) {
+    Service.act({role: 'npm', cmd: 'registryUnsubscribe'}, function (err, respond) {
+      expect(err).to.not.exist()
       expect(respond).to.exist()
+      done(err)
+    })
+  })
+  it('Registry Download Responds', function (done) {
+    Service.act({role: 'npm', cmd: 'registryDownload'}, function (err, respond) {
+      expect(err).to.not.exist()
+      expect(respond).to.exist()
+      done(err)
+    })
+  })
+  it('Feed is not started twice', function (done) {
+    Service.act({role: 'npm', cmd: 'registrySubscribe'}, function () {})
+    Service.act({role: 'npm', cmd: 'registrySubscribe'}, function (err, respond) {
+      expect(respond.message).to.equal('already running')
+      done(err)
+    })
+  })
+  it('Feed is not stopped twice', function (done) {
+    Service.act({role: 'npm', cmd: 'registryUnsubscribe'}, function () {})
+    Service.act({role: 'npm', cmd: 'registryUnsubscribe'}, function (err, respond) {
+      expect(respond.message).to.equal('already stopped')
       done(err)
     })
   })
