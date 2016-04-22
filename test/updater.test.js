5b1f38d1feb2fc93ea47d89fb9f48677a3bf435e
Gege Pincin <gege@pincin.com>
2016-04-21 22:32:11 -0400

    100% test coverage

--- test/updater.test.js
+++ test/updater.test.js
@@ -7,6 +7,9 @@ var Sinon = require('sinon')
 var Proxy = require('proxyquire')
 var Through2 = require('through2')
 
+var Emitter = require('events').EventEmitter
+var Inherits = require('util').inherits
+
 var npmStatsObj = {list: function () {}}
 Sinon.stub(npmStatsObj, 'list', function () {
   var stream = Through2()
@@ -19,68 +22,127 @@ Sinon.stub(npmStatsObj, 'list', function () {
   })
   return stream
 })
-var npmStats = Sinon.stub().returns(npmStatsObj)
 
-var Dequeue = Proxy('../lib/updater', {'npm-stats': npmStats})
+var lastFeed
+function Feed () {
+  Emitter.call(this)
+  this.start = () => {
+    this.emit('start')
+  }
+  this.stop = () => {
+    this.emit('stop')
+  }
+  lastFeed = this
+}
+
+Inherits(Feed, Emitter)
+
+var npmStats = Sinon.stub().returns(npmStatsObj)
+var Dequeue = Proxy('../lib/updater', {'npm-stats': npmStats, 'follow': {Feed: Feed}})
 
 var lab = exports.lab = Lab.script()
 var expect = Code.expect
-var before = lab.before
-var suite = lab.suite
+var beforeEach = lab.beforeEach
 var it = lab.it
 var describe = lab.describe
 
-var Service = Seneca()
-Service.use(Dequeue)
-
-suite('nodezoo-updater suite tests ', function () {
-  before({}, function (done) {
-    Service.ready(function (err) {
+var si
+function createSi (opts) {
+  var options = opts || {}
+  beforeEach({}, function (done) {
+    si = Seneca()
+    si.use(Dequeue, options)
+    si.ready(function (err) {
       if (err) {
         return process.exit(!console.error(err))
       }
-      Service.add('role:updater,info:update', function (msg, done) {
+      si.add('role:updater,info:update', function (msg, done) {
         done()
       })
       done(err)
     })
   })
-})
+}
 
-describe('nodezoo-updater tests', () => {
+describe('Calls "role:npm,cmd:registrySubscribe"', () => {
+  createSi()
   it('Registry Subscribe Responds', function (done) {
-    Service.act({role: 'npm', cmd: 'registrySubscribe'}, function (err, respond) {
-      expect(err).to.not.exist()
-      expect(respond).to.exist()
-      done(err)
-    })
-  })
-  it('Registry Unsubscribe Responds', function (done) {
-    Service.act({role: 'npm', cmd: 'registryUnsubscribe'}, function (err, respond) {
-      expect(err).to.not.exist()
-      expect(respond).to.exist()
-      done(err)
-    })
-  })
-  it('Registry Download Responds', function (done) {
-    Service.act({role: 'npm', cmd: 'registryDownload'}, function (err, respond) {
+    si.act({role: 'npm', cmd: 'registrySubscribe'}, function (err, respond) {
       expect(err).to.not.exist()
       expect(respond).to.exist()
       done(err)
     })
   })
   it('Feed is not started twice', function (done) {
-    Service.act({role: 'npm', cmd: 'registrySubscribe'}, function () {})
-    Service.act({role: 'npm', cmd: 'registrySubscribe'}, function (err, respond) {
+    si.act({role: 'npm', cmd: 'registrySubscribe'}, function () {})
+    si.act({role: 'npm', cmd: 'registrySubscribe'}, function (err, respond) {
       expect(respond.message).to.equal('already running')
       done(err)
     })
   })
+})
+
+describe('Calls "role:npm,cmd:registryUnsubscribe"', () => {
+  createSi()
+  it('Registry Unsubscribe Responds', function (done) {
+    si.act({role: 'npm', cmd: 'registrySubscribe'}, function () {})
+    si.act({role: 'npm', cmd: 'registryUnsubscribe'}, function (err, respond) {
+      expect(err).to.not.exist()
+      expect(respond).to.exist()
+      done(err)
+    })
+  })
   it('Feed is not stopped twice', function (done) {
-    Service.act({role: 'npm', cmd: 'registryUnsubscribe'}, function () {})
-    Service.act({role: 'npm', cmd: 'registryUnsubscribe'}, function (err, respond) {
+    si.act({role: 'npm', cmd: 'registryUnsubscribe'}, function () {})
+    si.act({role: 'npm', cmd: 'registryUnsubscribe'}, function (err, respond) {
       expect(respond.message).to.equal('already stopped')
       done(err)
     })
+  })
+})
+
+describe('Calls "role:npm,cmd:registryDownload without Opts"', () => {
+  createSi()
+  it('Registry Downloads Everything', function (done) {
+    si.act({role: 'npm', cmd: 'registryDownload'}, function (err, respond) {
+      Sinon.stub(si, 'act')
+      expect(err).to.not.exist()
+      expect(respond).to.exist()
+      setImmediate(() => {
+        expect(si.act.callCount).to.equal(3)
+        done(err)
+      })
+    })
+  })
+})
+
+describe('Calls "role:npm,cmd:registryDownload With Opts"', () => {
+  createSi({updaterLimit: 2})
+  it('Registry Downloads a limit of 2', function (done) {
+    si.act({role: 'npm', cmd: 'registryDownload'}, function (err, respond) {
+      Sinon.stub(si, 'act')
+      expect(err).to.not.exist()
+      expect(respond).to.exist()
+      setImmediate(() => {
+        expect(si.act.callCount).to.equal(2)
+        done(err)
+      })
+    })
+  })
+})
+
+describe('Feed', () => {
+  createSi()
+  it('Processes Feed Error', function (done) {
+    lastFeed.on('stop', function () {
+      done()
+    })
+    lastFeed.emit('error')
+  })
+  it('Processes Feed Changes', function (done) {
+    Sinon.stub(si, 'act')
+    lastFeed.emit('change', {id: 'honeybadger'})
+    expect(si.act.calledWith('role:updater,info:update', {name: 'honeybadger'})).to.equal(true)
+    done()
   })
 })
